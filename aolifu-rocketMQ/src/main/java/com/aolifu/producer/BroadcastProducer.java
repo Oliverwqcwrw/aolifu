aa7a73439409a4a132104922e70ef3da47e8e24a
Oliver <wqdyxnbd@163.com>
2022-03-28 13:40:46 +0800

    init file

---
+++ aolifu-rocketMQ/src/main/java/com/aolifu/producer/BroadcastProducer.java
@@ -0,0 +1,24 @@
+package com.aolifu.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class BroadcastProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("BROADCAST_GROUP");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+
+        for (int i = 0; i < 10; i++){
+            Message msg = new Message("BROADCAST_TOPIC",
+                "TagA",
+                "OrderID188",
+                "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
