65c0a4218c8da5d4cdb29c83c6b43ffa335c5917
Oliver <wqdyxnbd@163.com>
2022-04-27 09:25:06 +0800

    update starter

---
+++ aolifu-mq-starter/src/main/java/com/aolifu/example/ExampleConsumer.java
@@ -0,0 +1,52 @@
+package com.aolifu.example;
+
+import com.aolifu.consumer.RocketMQConsumer;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.springframework.context.annotation.Bean;
+
+public class ExampleConsumer {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    /**
+     * 默认消费者
+     * @return
+     */
+    @Bean
+    public RocketMQConsumer rocketMQConsumer(){
+        RocketMQConsumer consumer = new RocketMQConsumer();
+        final DefaultMQPushConsumer pushConsumer = consumer.getConsumer();
+        pushConsumer.setNamesrvAddr("127.0.0.1:9876");
+        pushConsumer.setMaxReconsumeTimes(Integer.MAX_VALUE);
+        pushConsumer.registerMessageListener((MessageListenerConcurrently) (list, consumeConcurrentlyContext) -> {
+            System.out.println();
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        return consumer;
+    }
+
+    /**
+     * legion消费者
+     * @return
+     */
+    @Bean
+    public RocketMQConsumer legionRocketMQConsumer(){
+        RocketMQConsumer consumer = new RocketMQConsumer();
+        final DefaultMQPushConsumer pushConsumer = consumer.getConsumer();
+        pushConsumer.setNamesrvAddr("127.0.0.1:9877");
+        pushConsumer.setMaxReconsumeTimes(100);
+        pushConsumer.setConsumeThreadMax(20);
+        pushConsumer.setConsumeThreadMin(5);
+        pushConsumer.setVipChannelEnabled(false);
+        pushConsumer.registerMessageListener((MessageListenerConcurrently) (list, consumeConcurrentlyContext) -> {
+            System.out.println("legion 消费者消费" + list.get(0));
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        return consumer;
+    }
+}
--- aolifu-mq-starter/src/main/java/com/aolifu/example/ExampleProducer.java
+++ aolifu-mq-starter/src/main/java/com/aolifu/example/ExampleProducer.java
@@ -6,8 +6,9 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.context.annotation.Bean;
 
-public class ExampleProducer extends RocketMQProducer {
+public class ExampleProducer extends RocketMQProducer{
 
     public static void main(String[] args) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
         RocketMQProducer producer = new RocketMQProducer();
@@ -16,4 +17,31 @@ public class ExampleProducer extends RocketMQProducer {
         defaultMQProducer.setNamesrvAddr("127.0.0.1:9876");
         producer.sendMessage(new Message());
     }
+
+    /**
+     * 默认生产者
+     * @return
+     */
+    @Bean
+    public RocketMQProducer rocketMQProducer(){
+        RocketMQProducer producer = new RocketMQProducer();
+        final DefaultMQProducer defaultMQProducer = producer.getProducer();
+        // 自定义设置参数 和配置中心解耦 支持每一个生产者连接个性化的nameserver
+        defaultMQProducer.setNamesrvAddr("127.0.0.1:9876");
+        return producer;
+    }
+
+    /**
+     * legion生产者
+     * @return
+     */
+    @Bean
+    public RocketMQProducer legionRocketMQProducer(){
+        RocketMQProducer producer = new RocketMQProducer();
+        final DefaultMQProducer defaultMQProducer = producer.getProducer();
+        // 自定义设置参数 和配置中心解耦 支持每一个生产者连接个性化的nameserver
+        defaultMQProducer.setNamesrvAddr("127.0.0.1:9877");
+        return producer;
+    }
+
 }
