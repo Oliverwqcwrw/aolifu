fdf95c723375f8116c830fb5c83350ae3410dc1b
Oliver <wqdyxnbd@163.com>
2022-04-29 09:22:59 +0800

    add dynamic tp

---
+++ aolifu-dynamic-tp/src/main/java/com/aolifu/App.java
@@ -0,0 +1,17 @@
+package com.aolifu;
+
+import cn.hippo4j.core.enable.EnableDynamicThreadPool;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableDynamicThreadPool
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ aolifu-dynamic-tp/src/main/java/com/aolifu/config/ThreadPoolConfig.java
@@ -0,0 +1,32 @@
+package com.aolifu.config;
+
+import cn.hippo4j.core.executor.DynamicThreadPool;
+import cn.hippo4j.core.executor.support.ResizableCapacityLinkedBlockIngQueue;
+import cn.hippo4j.core.executor.support.ThreadPoolBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    @DynamicThreadPool
+    public ThreadPoolExecutor dynamicThreadPoolExecutor() {
+        String threadPoolId = "message-consume";
+        ThreadPoolExecutor dynamicExecutor = ThreadPoolBuilder.builder()
+                .threadFactory(threadPoolId)
+                .threadPoolId(threadPoolId)
+                .corePoolSize(5)
+                .maxPoolNum(10)
+                .workQueue(new ResizableCapacityLinkedBlockIngQueue(1024))
+                .rejected(new ThreadPoolExecutor.AbortPolicy())
+                .keepAliveTime(6000, TimeUnit.MILLISECONDS)
+                .dynamicPool()
+                .build();
+        return dynamicExecutor;
+    }
+
+}
