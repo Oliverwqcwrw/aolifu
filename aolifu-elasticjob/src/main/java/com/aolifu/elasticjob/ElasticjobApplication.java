8437a234d51f83e0d13c83329a967f5336c12b38
Oliver <wqdyxnbd@163.com>
2022-06-07 14:16:02 +0800

    Add elasticjob

---
+++ aolifu-elasticjob/src/main/java/com/aolifu/elasticjob/ElasticjobApplication.java
@@ -0,0 +1,13 @@
+package com.aolifu.elasticjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticjobApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticjobApplication.class, args);
+    }
+    
+}
---
+++ aolifu-elasticjob/src/main/java/com/aolifu/elasticjob/lite/MyJob.java
@@ -0,0 +1,28 @@
+package com.aolifu.elasticjob.lite;
+
+import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyJob implements SimpleJob {
+    
+    @Override
+    public void execute(ShardingContext context) {
+        switch (context.getShardingItem()) {
+            case 0: 
+                // do something by sharding item 0
+                System.out.println("sharding item 0");
+                break;
+            case 1: 
+                // do something by sharding item 1
+                System.out.println("sharding item 1");
+                break;
+            case 2: 
+                // do something by sharding item 2
+                System.out.println("sharding item 2");
+                break;
+            // case n: ...
+        }
+    }
+}
