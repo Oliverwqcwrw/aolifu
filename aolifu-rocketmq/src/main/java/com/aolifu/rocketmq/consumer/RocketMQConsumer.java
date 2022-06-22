7797422bdfbf1f6f59da465cf504fe6368cf05d2
Oliver <wqdyxnbd@163.com>
2022-06-22 11:53:01 +0800

    Update RocketMQ module

--- aolifu-rocketmq/src/main/java/com/aolifu/rocketmq/consumer/RocketMQConsumer.java
+++ aolifu-rocketmq/src/main/java/com/aolifu/rocketmq/consumer/RocketMQConsumer.java
@@ -7,17 +7,27 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 public class RocketMQConsumer  implements ApplicationRunner {
-
+    
     private DefaultMQPushConsumer consumer = new DefaultMQPushConsumer();
-
-
+    
+    
     @Override
     public void run(ApplicationArguments args) throws MQClientException {
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
         consumer.setMaxReconsumeTimes(10);
+        // 最大消费消息数量
+        consumer.setConsumeMessageBatchMaxSize(10);
+        // 消费组
+        consumer.setConsumerGroup("DEFAULT_GROUP");
+        // 设置最大消费线程数
+        consumer.setConsumeThreadMax(20);
+        // 设置拉取消息的数量
+        consumer.setPullBatchSize(32);
+        // 设置拉取请求间隔
+        consumer.setPullInterval(0);
         consumer.start();
     }
-
+    
     public DefaultMQPushConsumer getConsumer(){
         return consumer;
     }
--- aolifu-rocketmq/src/main/java/com/aolifu/rocketmq/producer/RocketMQProducer.java
+++ aolifu-rocketmq/src/main/java/com/aolifu/rocketmq/producer/RocketMQProducer.java
@@ -10,28 +10,46 @@ import org.apache.rocketmq.common.protocol.ResponseCode;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 public class RocketMQProducer {
-
+    
     private DefaultMQProducer producer = new DefaultMQProducer();
-
+    
     @PostConstruct
     public void start() throws MQClientException {
         // 失败默认重试次数
         producer.setRetryTimesWhenSendFailed(10);
         // 发送消息默认超时时间
         producer.setSendMsgTimeout(5000);
+        producer.setProducerGroup("DEFAULT_PRODUCER_GROUP");
+        // 消息体超过4K启用压缩
+        producer.setCompressMsgBodyOverHowmuch(1024 * 4);
+        // 发送失败换一个broker重试
+        producer.setRetryAnotherBrokerWhenNotStoreOK(true);
+        // 消息体允许的最大值 4M
+        producer.setMaxMessageSize(1024 * 1024 * 4);
+        // 队列数
+        producer.setDefaultTopicQueueNums(12);
         producer.start();
     }
-
+    
     public DefaultMQProducer getProducer() {
         return producer;
     }
-
+    
     public void sendMessage(Message message) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
         SendResult sendResult = producer.send(message);
         if (sendResult == null || sendResult.getSendStatus() != SendStatus.SEND_OK) {
             throw new MQClientException(ResponseCode.SYSTEM_ERROR,"消息发送失败");
+        }
+    }
+    
+    public void sendBatchMessage(List<Message> messageList) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        SendResult sendResult = producer.send(messageList);
+        
+        if (sendResult == null || sendResult.getSendStatus() != SendStatus.SEND_OK) {
+            throw new MQClientException(ResponseCode.SYSTEM_ERROR,"批量消息发送失败");
         }
     }
 }
